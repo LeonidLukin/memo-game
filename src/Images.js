dd6eba8536635ccc0006f5acccf8b91e7216b62d
Leonid Lukin <leonidlukin90@gmail.com>
2021-09-05 15:27:13 +0300

    created memp-cards and styled them

---
+++ src/Images.js
@@ -0,0 +1,31 @@
+import Image01 from './images/01.jpg'
+import Image02 from './images/02.jpg'
+import Image03 from './images/03.jpg'
+import Image04 from './images/04.jpg'
+import Image05 from './images/05.jpg'
+import Image06 from './images/06.jpg'
+import Image07 from './images/07.jpg'
+import Image08 from './images/08.jpg'
+import Image09 from './images/09.jpg'
+import Image10 from './images/10.jpg'
+import Image11 from './images/11.jpg'
+import Image12 from './images/12.jpg'
+
+
+
+
+export default [
+    Image01,
+    Image02,
+    Image03,
+    Image04,
+    Image05,
+    Image06,
+    Image07,
+    Image08,
+    Image09,
+    Image10,
+    Image11,
+    Image12,
+
+]
